10e1f143ca0143987c1b2b75354c4ff478b71676
Nayyab Zahra <nayyab10122004@gmail.com>
2026-05-18 07:04:20 -0700

    profile

---
+++ Script.js
@@ -0,0 +1,31 @@
+// script.js
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Smooth reveal animation
+
+const cards = document.querySelectorAll(
+    ".skill-card, .project-card, .cert-card, .contact-box, .info-box"
+);
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
+
+cards.forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = "0.6s";
+});
